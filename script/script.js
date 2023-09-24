3d63526ca45b3e021b63011ad3a527acaf101afc
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2023-09-24 18:12:59 +0200

    js improve

--- script/script.js
+++ script/script.js
@@ -7,5 +7,19 @@ function menuPr(){
 }
 
 function newMessage(){
-    
+
+}
+
+window.onscroll = function(){headerScroll()}
+
+function headerScroll(){
+    var logo = document.querySelector('#logo')
+    const logoWidth = window.getComputedStyle(logo)
+    var scroll = document.documentElement.scrollTop
+    if (logo != 100){
+        document.querySelector('#logo').style.width = logo - scroll
+    }
+
+    console.log(logoWidth.width)
+    console.log(scroll)
 }
